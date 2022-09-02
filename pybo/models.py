e1296ff8bcb4a9074c434c95e0a6289042cb8e62
jupyter1234 <wntjdals0412@gmail.com>
2022-09-02 14:33:32 +0900

    Initial commit

---
+++ pybo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+#qustionModel
+class Question(models.Model):
+    subject = models.CharField(max_length=200)   #제목
+    content = models.TextField() #내용
+    create_date = models.DateTimeField()  #작성일시
+
+    def __str__(self):
+        return self.subject
+
+#answermodel
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)   #question과 연결됨
+    content = models.TextField()
+    create_date = models.DateTimeField()
